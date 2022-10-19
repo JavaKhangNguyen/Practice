932d304dd5027bcc47e34603597de889c6c508cd
JavaKhangNguyen <khang2073@gmail.com>
2022-10-19 09:59:47 +0700

    Practice

--- Test/Test.java
+++ Test/Test.java
@@ -1,10 +1,9 @@
-package Java;
+package Test;
 //Import library here
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        //Test anything here
-		
+        
     }
 }
