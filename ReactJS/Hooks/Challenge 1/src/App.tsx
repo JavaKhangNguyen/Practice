f49751e34e30433bbe5cc98472d6ac0de5eebb6e
Nguyễn Phúc Khang <khang2073@gmail.com>
2024-10-21 11:45:47 +0700

    Complete practice Challenge 1

---
+++ ReactJS/Hooks/Challenge 1/src/App.tsx
@@ -0,0 +1,15 @@
+import  { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [show, setShow] = useState(true)
+  
+  return (
+    <div>
+      <button onClick={() => {setShow(!show)}}>Show/hide</button>
+      {show && <h1>Hello world!</h1>}
+    </div>
+  )
+}
+
+export default App
